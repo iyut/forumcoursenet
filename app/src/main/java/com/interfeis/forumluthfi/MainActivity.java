d8ea16a3d76246fb09bd9a77f366be90fd8745ba
Luthfi Bintoro <iyut85@yahoo.com>
2019-02-06 15:32:30 +0700

    adding login and register activity

--- app/src/main/java/com/interfeis/forumluthfi/MainActivity.java
+++ app/src/main/java/com/interfeis/forumluthfi/MainActivity.java
@@ -1,13 +1,107 @@
 package com.interfeis.forumluthfi;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActionBarDrawerToggle left_menu_toggle;
+
+    boolean clickExit = false;
+
+    SharedPreferences data_app;
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        left_menu_toggle.syncState();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        openAppFragment( new HomeFragment() );
+
+        data_app = getSharedPreferences("DATA_APP", MODE_PRIVATE);
+
+        final DrawerLayout main_act_layout = (DrawerLayout) findViewById(R.id.layout_main_activity);
+
+        NavigationView left_menu = (NavigationView) findViewById(R.id.nv_left_menu);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        left_menu_toggle = new ActionBarDrawerToggle( MainActivity.this,
+                main_act_layout,
+                (R.string.open),
+                (R.string.close)
+        );
+
+        main_act_layout.addDrawerListener(left_menu_toggle);
+
+        left_menu.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                if(menuItem.getItemId() == R.id.left_menu_home){
+
+                    openAppFragment(new HomeFragment());
+
+                    Toast.makeText(getApplicationContext(), getString(R.string.home), Toast.LENGTH_SHORT).show();
+                    main_act_layout.closeDrawers();
+
+                }else if(menuItem.getItemId() == R.id.left_menu_logout){
+
+                    removeSharedData();
+
+                    Intent i = new Intent(getApplicationContext(), LoginActivity.class);
+                    startActivity(i);
+
+                    finish();
+                }
+
+                return false;
+            }
+        });
+
+    }
+
+    public void openAppFragment(Fragment fragment){
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.frm_main_activity, fragment)
+                .commit();
+    }
+
+    public void removeSharedData(){
+        getSharedPreferences("DATA_APP", MODE_PRIVATE).edit().clear().commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        left_menu_toggle.onOptionsItemSelected(item);
+
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/interfeis/forumluthfi/RegisterActivity.java
+++ app/src/main/java/com/interfeis/forumluthfi/RegisterActivity.java
@@ -7,37 +7,55 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class LoginActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_register);
     }
 
-    public void btnLoginClicked(View view) {
+    public void btnBacktologinClicked(View view) {
+
+        Intent i = new Intent(getApplicationContext(), LoginActivity.class);
+        startActivity(i);
+    }
+
+    public void btnSubmitClicked(View view) {
+        TextInputEditText txtFullname = (TextInputEditText) findViewById(R.id.txtFullname);
+
         TextInputEditText txtUsername = (TextInputEditText) findViewById(R.id.txtUsername);
         TextInputEditText txtPassword = (TextInputEditText) findViewById(R.id.txtPassword);
 
+        String strFullname = txtFullname.getText().toString();
         String strUsername = txtUsername.getText().toString();
         String strPassword = txtPassword.getText().toString();
 
-        if(strUsername.length() == 0){
+        if (strFullname.length() == 0) {
+
+            txtFullname.setError(getString(R.string.full_name_required));
+
+        } else if (strFullname.length() < 4) {
+
+            txtFullname.setError(getString(R.string.full_name_must_longer));
+
+        } else if (strUsername.length() == 0) {
 
             txtUsername.setError(getString(R.string.email_required));
 
-        }else if(!Helper.isEmailValid(strUsername)){
+        } else if (!Helper.isEmailValid(strUsername)) {
 
             txtUsername.setError(getString(R.string.email_not_valid));
 
-        }else if(strPassword.length() == 0){
+        } else if (strPassword.length() == 0) {
 
             txtPassword.setError(getString(R.string.password_required));
 
-        }else{
+        } else {
 
             SharedPreferences.Editor data_app = getSharedPreferences("DATA_APP", MODE_PRIVATE).edit();
 
+            data_app.putString("data_fullname", strFullname);
             data_app.putString("data_email", strUsername);
             data_app.commit();
 
@@ -46,11 +64,5 @@ public class LoginActivity extends AppCompatActivity {
             startActivity(i);
             finish();
         }
-    }
-
-    public void btnSignupClicked(View view) {
-
-        Intent i = new Intent(getApplicationContext(), RegisterActivity.class);
-        startActivity(i);
     }
 }
